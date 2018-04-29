272489cdbeac25c150fac804c73131199285595c
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-29 02:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,16 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(num) {
   var newArray = [];
+<<<<<<< HEAD
   do {
     newArray.push('I love the Beatles!');
     num++;
+=======
+  var i = 0;
+  do {
+    newArray.push('I love the Beatles!');
+    i++;
+>>>>>>> a9e56d8e6e84c1a75177b9f8b75a8e8bd634e9e6
   } while
   (num < 15);
   return newArray;
